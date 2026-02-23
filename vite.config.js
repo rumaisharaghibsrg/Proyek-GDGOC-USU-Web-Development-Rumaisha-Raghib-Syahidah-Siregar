c199ff330617e84a2b4dda3ec2754e2d0215233b
rumaisharaghibsrg <rumaisharaghib@gmail.com>
2026-02-23 13:25:54 +0700

    Update konfigurasi homepage dan base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/Proyek-GDGOC-USU-Web-Development-Rumaisha-Raghib-Syahidah-Siregar/",
   plugins: [
     react(),
     tailwindcss()
